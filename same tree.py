e8ef73d0555f95ad08cab55d0e87a5fe4790962b
cjpeter <zjucjpeter@gmail.com>
2015-08-13 14:26:22 -0400

    Create same tree.py
    Given two binary trees, write a function to check if they are equal or not.
    
    Two binary trees are considered equal if they are structurally identical and the nodes have the same value.

---
+++ same tree.py
@@ -0,0 +1,111 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {boolean}
+    def isSameTree(self, p, q):
+        if p == None:
+            return q == None
+        if q == None:
+            return p == None
+        if p.val != q.val:
+            return False
+        else:    
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        
+        # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {boolean}
+    def isSameTree(self, p, q):
+        if p == None:
+            return q == None
+        if q == None:
+            return p == None
+        if p.val != q.val:
+            return False
+        else:    
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        
+        # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {boolean}
+    def isSameTree(self, p, q):
+        if p == None:
+            return q == None
+        if q == None:
+            return p == None
+        if p.val != q.val:
+            return False
+        else:    
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        
+        # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {boolean}
+    def isSameTree(self, p, q):
+        if p == None:
+            return q == None
+        if q == None:
+            return p == None
+        if p.val != q.val:
+            return False
+        else:    
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        
+        # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {boolean}
+    def isSameTree(self, p, q):
+        if p == None:
+            return q == None
+        if q == None:
+            return p == None
+        if p.val != q.val:
+            return False
+        else:    
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        
+        
